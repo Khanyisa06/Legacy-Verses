9a07fe90bd0641dcfb2c03a7e4d16a49b356b297
Khanyisa06 <khosavhutomi3@gmail.com>
2023-06-15 00:20:31 +0200

    First Commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegacyVerses.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Trust()
+        {
+            return View();
+        }
+        public IActionResult Faith()
+        {
+            return View();
+        }
+        public IActionResult Forgiveness()
+        {
+            return View();
+        }
+        public IActionResult Love()
+        {
+            return View();
+
+        }
+        public IActionResult Patience()
+        {
+            return View();
+        }
+        public IActionResult Peace()
+        {
+            return View();
+        }
+        public IActionResult Strength()
+        {
+            return View();
+        }
+    }
+}
